e83674bfe276695121c7c53feb6a1c2b305ca847
nareshtelkar <nareshtelkar2674@gmail.com>
2025-02-04 19:49:21 +0530

    commit

--- src/Root/Home.jsx
+++ src/Root/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
   return (
     <div className='home'>
       <h1>Welcome to our Employee Management System</h1>
+      <img src="https://thumbs.dreamstime.com/b/smiling-group-co-workers-standing-office-41862952.jpg" alt="" />
     </div>
     
     
--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,26 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, Outlet } from 'react-router-dom'
+import '../App.css'
 
 const Root = () => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen)
+  }
+
   return (
     <div className='root'>
-      <div className='left'>
+      <div className={`left ${isOpen ? 'active' : ''}`}>
         <h1>EMPLOYEE MANAGEMENT</h1>
-        <ul>
+        <div className="hamburger" onClick={toggleMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <ul className={isOpen ? 'show' : ''}>
           <li>
-            <Link to='/root/home'>Home</Link>
+            <Link to='/root/home' onClick={() => setIsOpen(false)}>Home</Link>
           </li>
           <li>
-            <Link to='/root/registration'>Registration</Link>
+            <Link to='/root/registration' onClick={() => setIsOpen(false)}>Registration</Link>
           </li>
           <li>
-            <Link to='/root/employees'>Employees</Link>
+            <Link to='/root/employees' onClick={() => setIsOpen(false)}>Employees</Link>
           </li>
           <li>
-            <Link to='/root/contact'>Contact</Link>
+            <Link to='/root/contact' onClick={() => setIsOpen(false)}>Contact</Link>
           </li>
           <li>
-            <Link to='/login'>Logout</Link>
+            <Link to='/login' onClick={() => setIsOpen(false)}>Logout</Link>
           </li>
         </ul>
       </div>
@@ -28,8 +40,6 @@ const Root = () => {
       <div className='right'>
         <Outlet />
       </div>
-
-
     </div>
   )
 }
